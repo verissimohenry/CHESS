b064d684e2185e7a1d9202e92d911b23b04b379b
HENRY VERISSIMO <verissimohenry04@gmail.com>
2023-01-17 16:55:33 +0100

    Modify chess.js file  add coloring function

--- Chess.js
+++ Chess.js
@@ -20,3 +20,27 @@ function insertImage() {
 }
 insertImage()
 
+//Coloring
+
+function coloring() {
+    const color = document.querySelectorAll('.box')
+
+    color.forEach(color => {
+
+        getId = color.id
+        arr = Array.from(getId)
+        arr.shift()
+        aside = eval(arr.pop())
+        aup = eval(arr.shift())
+        a = aside + aup
+
+        if (a % 2 == 0) {
+            color.style.backgroundColor = 'rgb(240, 201, 150)'
+        }
+        if (a % 2 !== 0) {
+            color.style.backgroundColor = 'rgb(100, 75, 43)'
+        }
+
+    })
+}
+coloring()
