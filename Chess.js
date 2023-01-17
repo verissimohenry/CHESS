af5f8e9342d6fff1439f2ad84956999804545cc3
HENRY VERISSIMO <verissimohenry04@gmail.com>
2023-01-17 17:07:09 +0100

    Modify chess.js file knight  function

--- Chess.js
+++ Chess.js
@@ -430,3 +430,39 @@ ROOK
                 item.style.backgroundColor = 'pink'
 
             }
+
+         // KNIGHT
+
+         if (item.innerText == `${toggle}knight`) {
+
+
+            if (aside < 7 && aup < 800) {
+                document.getElementById(`b${a + 100 + 2}`).style.backgroundColor = 'green'
+            }
+            if (aside < 7 && aup > 200) {
+                document.getElementById(`b${a - 100 + 2}`).style.backgroundColor = 'green'
+            }
+            if (aside < 8 && aup < 700) {
+                document.getElementById(`b${a + 200 + 1}`).style.backgroundColor = 'green'
+            }
+            if (aside > 1 && aup < 700) {
+                document.getElementById(`b${a + 200 - 1}`).style.backgroundColor = 'green'
+            }
+            if (aside > 2 && aup < 800) {
+                document.getElementById(`b${a - 2 + 100}`).style.backgroundColor = 'green'
+            }
+            if (aside > 2 && aup > 100) {
+                document.getElementById(`b${a - 2 - 100}`).style.backgroundColor = 'green'
+            }
+            if (aside < 8 && aup > 200) {
+                document.getElementById(`b${a - 200 + 1}`).style.backgroundColor = 'green'
+            }
+            if (aside > 1 && aup > 200) {
+                document.getElementById(`b${a - 200 - 1}`).style.backgroundColor = 'green'
+            }
+
+            item.style.backgroundColor = 'pink'
+
+        }
+    }
+
